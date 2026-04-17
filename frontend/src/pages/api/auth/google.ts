b3d99cc7bec14706577c49d4d2b2589ec6377496
Mahad-Nawaz-Khan <mahadnawaz2006@gmail.com>
2026-04-17 23:25:55 +0500

    Changed API Routes

--- frontend/src/pages/api/auth/google.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-import { authenticateProvider } from "@/lib/auth-api";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return authenticateProvider("google", req, res);
-}
